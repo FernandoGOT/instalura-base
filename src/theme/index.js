2686080ba0cbbe5d08e606affda5151e034ede3a
FernandoGOT <fernando.1f@hotmail.com>
2021-02-14 02:36:37 -0300

    refactor(theme): added the breakpoints values to the theme
    Added a object with all breakpoints and their values, this is the base for the responssive part of
    the app

--- src/theme/index.js
+++ src/theme/index.js
@@ -41,8 +41,18 @@ export const colors = {
   },
 }
 
+export const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+}
+
+
 export default {
   colors,
+  breakpoints,
   typographyVariants,
   borderRadius: '12px',
   transition: '200ms ease-in-out',
