e6180a33fb6ccc18ea29f7ff8c3587988a301baf
FernandoGOT <fernando.1f@hotmail.com>
2021-02-16 05:50:15 -0300

    feat(components > layout > box): created the Box component
    Created the Box component, this component is meant to be used in the place of a div and his only
    feature is to receive css props as jsx props

---
+++ src/components/layout/Box/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import propToStyle from '../../../theme/utils/propToStyle'
+
+const Box = styled.div`
+  ${propToStyle(`flex`)}
+  ${propToStyle(`display`)}
+  ${propToStyle(`flexWrap`)}
+  ${propToStyle(`flexDirection`)}
+  ${propToStyle(`justifyContent`)}
+  ${propToStyle(`backgroundImage`)}
+  ${propToStyle(`backgroundRepeat`)}
+  ${propToStyle(`backgroundPosition`)}
+`
+
+export default Box
