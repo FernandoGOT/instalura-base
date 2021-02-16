c2594091711267484e1f101a7cd0fdb62c30e5b9
FernandoGOT <fernando.1f@hotmail.com>
2021-02-16 02:20:35 -0300

    feat(theme > utils > proptostyle): created the propToStyle function
    Created the propToStyle function, this function is used to convert props from the jsx and convert to
    a valid css

---
+++ src/theme/utils/propToStyle.js
@@ -0,0 +1,42 @@
+import { css } from 'styled-components'
+
+import breakpointsMedia from './breakpointsMedia'
+
+const propToStyle = propName => props => {
+  const propValue = props[propName]
+
+  if (typeof propValue === `object`) {
+    return css`
+      ${breakpointsMedia({
+      ...(propValue.xs && {
+        xs: { [propName]: propValue.xs },
+      }),
+      ...(propValue.sm && {
+        sm: { [propName]: propValue.sm },
+      }),
+      ...(propValue.md && {
+        md: { [propName]: propValue.md },
+      }),
+      ...(propValue.lg && {
+        lg: { [propName]: propValue.lg },
+      }),
+      ...(propValue.xl && {
+        xl: { [propName]: propValue.xl },
+      }),
+    })}
+    `
+    // return breakpointsMedia({
+    //   xs: { [propName]: propValue.xs },
+    //   sm: { [propName]: propValue.sm },
+    //   md: { [propName]: propValue.md },
+    //   lg: { [propName]: propValue.lg },
+    //   xl: { [propName]: propValue.xl }
+    // })
+  }
+
+  return {
+    [propName]: props[propName]
+  }
+}
+
+export default propToStyle
