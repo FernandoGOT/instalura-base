814a649d6fd23812b7df3ce71d0f0b1d2a5945e4
FernandoGOT <fernando.1f@hotmail.com>
2021-02-16 05:54:42 -0300

    feat(index): implemented the Box component and updated the Grid props
    Implemented the Box component and updated the Grid components, to receive some css props as jsx
    props

--- pages/index.js
+++ pages/index.js
@@ -3,60 +3,68 @@ import Text from '../src/components/foundation/Text'
 import Button from '../src/components/commons/Button'
 import Footer from '../src/components/commons/Footer'
 import Grid from '../src/components/layout/Grid'
+import Box from '../src/components/layout/Box'
 
 
 export default function Home() {
   return (
-    <div style={{
-      flex: '1',
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
+    <Box
+      flex={1}
+      display='flex'
+      flexWrap='wrap'
+      flexDirection='column'
+      backgroundRepeat='no-repeat'
+      justifyContent='space-between'
+      backgroundPosition='bottom right'
+      backgroundImage='url(/images/bubbles.svg)'
+    >
       <Menu />
-      <Grid.Container>
+      <Grid.Container
+        marginTop={{ xs: '32px', md: '75px' }}
+      >
         <Grid.Row>
           <Grid.Col
-            offset={1}
+            offset={{ xs: 0, md: 1 }}
             value={{ xs: 12, md: 5 }}
+            display='flex'
+            flexDirection='column'
+            alignItems='flex-start'
+            justifyContent='center'
           >
-            <div>
-              <Text
-                variant="title"
-                tag="h1"
-                color="tertiary.main"
-                textAlign={{
-                  xs: 'center',
-                  md: 'left',
-                }}
-              >
-                Compartilhe momentos e conecte-se com amigos
+            <Text
+              variant="title"
+              tag="h1"
+              color="tertiary.main"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Compartilhe momentos e conecte-se com amigos
               </Text>
-              <Text
-                variant="paragraph1"
-                tag="p"
-                color="tertiary.light"
-                textAlign={{
-                  xs: 'center',
-                  md: 'left',
-                }}
-              >
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.light"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
               </Text>
 
-              <Button
-                variant="primary.main"
-                margin={{
-                  xs: 'auto',
-                  md: 'initial',
-                }}
-                display="block"
-              >
-                Cadastrar
-              </Button>
-            </div>
+            <Button
+              variant="primary.main"
+              margin={{
+                xs: 'auto',
+                md: 'initial',
+              }}
+              display="block"
+            >
+              Cadastrar
+            </Button>
           </Grid.Col>
           <Grid.Col
             value={{ xs: 12, md: 6 }}
@@ -69,6 +77,6 @@ export default function Home() {
         </Grid.Row>
       </Grid.Container>
       <Footer />
-    </div>
+    </Box>
   )
 }
