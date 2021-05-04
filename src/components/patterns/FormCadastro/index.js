7290d739466ed06089963dc1160b2d8a72b9f54e
Fernando GOT <fernando.1f@hotmail.com>
2021-05-04 07:17:24 -0300

    feat(formcadastro): created the FormCadastro component
    Created the FormCadastro component to isolate the register modal form logic

---
+++ src/components/patterns/FormCadastro/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import Box from '../../layout/Box'
+import Grid from '../../layout/Grid'
+import Button from '../../commons/Button'
+import TextField from '../../forms/TextField'
+import Text from '../../foundation/Text'
+
+const FormContent = () => {
+  const [userInfo, setUserInfo] = React.useState({
+    usuario: 'fernandoGomes',
+    email: 'fernando@gmail.com'
+  })
+
+  const handleChange = (event) => {
+    const fieldName = event.target.getAttribute('name')
+
+    setUserInfo({
+      ...userInfo,
+      [fieldName]: event.target.value
+    })
+  }
+
+  const isFormInvalid = userInfo.usuario.length === 0 || userInfo.email.length === 0
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        console.log('O formulário está pronto, vamos cadastrar de fato o usuário')
+      }}
+    >
+      <Text variant="title" tag="h1" color="tertiary.main">
+        Pronto para saber da vida dos outros?
+      </Text>
+      <Text variant="paragraph1" tag="p" color="tertiary.light" marginBottom="32px">
+        Você está a um passo de saber tudoo que está rolando no bairro, complete seu cadastro agora!
+      </Text>
+      <div>
+        <TextField type="email" placeholder="Email" name="email" value={userInfo.email} onChange={handleChange} />
+      </div>
+      <div>
+        <TextField type="text" placeholder="Usuário" name="usuario" value={userInfo.usuario} onChange={handleChange} />
+      </div>
+      <Button type="submit" variant="primary.main" disabled={isFormInvalid} fullWidth>
+        Cadastrar
+      </Button>
+    </form>
+  )
+}
+
+const FormCadastro = ({ propsDoModal }) => (
+  <Grid.Row flex={1} marginLeft={0} marginRight={0} justifyContent="flex-end">
+    <Grid.Col display="flex" paddingRight={{ md: '0' }} flex={0} value={{ xs: 12, md: 5, lg: 4 }}>
+      <Box
+        boxShadow="-10px 0px 24px rgba(7, 12, 14, 0.1)"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        flex={1}
+        padding={{
+          xs: '16px',
+          md: '85px'
+        }}
+        backgroundColor="white"
+        {...propsDoModal}
+      >
+        <FormContent />
+      </Box>
+    </Grid.Col>
+  </Grid.Row>
+)
+
+export default FormCadastro
