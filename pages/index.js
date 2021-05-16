be2e0f0dfc3e91852cdc9f2d99c38558386eb3ac
Fernando GOT <fernando.1f@hotmail.com>
2021-05-16 00:24:28 -0300

    feat(faq): implemented the FAQ page
    Implemented the FAQ page and its dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Box from '../src/components/layout/Box'
-import Grid from '../src/components/layout/Grid'
+import Box from '../src/components/foundation/layout/Box'
+import Grid from '../src/components/foundation/layout/Grid'
 import Menu from '../src/components/commons/Menu'
 import Text from '../src/components/foundation/Text'
 import Button from '../src/components/commons/Button'
