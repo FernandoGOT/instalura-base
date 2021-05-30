01775b91f3c0bb4de05f0e1f1a29756b54dd0694
Fernando GOT <fernando.1f@hotmail.com>
2021-05-30 09:18:49 -0300

    feat(next.config): added headers
    Implemented a header to the next.config file, this way we can impede someone from using the app/*
    pages in wireframe

--- next.config.js
+++ next.config.js
@@ -4,5 +4,18 @@ module.exports = {
   trailingSlash: true,
   async redirects() {
     return redirects
+  },
+  async headers() {
+    return [
+      {
+        source: '/app/:path*/',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ]
   }
 }
