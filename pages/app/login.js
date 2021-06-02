005c662865d369217d9131533fd7d7a2e02e99af
Fernando GOT <fernando.1f@hotmail.com>
2021-06-02 12:43:47 -0300

    feat(login): added data-cy to inputs and button
    Added the prop "ddata-cy" to the form inputs and button to test on cypress

--- pages/app/login.js
+++ pages/app/login.js
@@ -12,11 +12,12 @@ import { WebsitePageContext } from '../../src/components/wrappers/WebsitePage'
 
 const LoginForm = () => (
   <form id="formCadastro" action="/app/profile">
-    <TextField placeholder="Usuário" name="usuario" />
-    <TextField placeholder="Senha" name="senha" type="password" />
+    <TextField placeholder="Usuário" name="usuario" data-cy="login-usuario" />
+    <TextField placeholder="Senha" name="senha" type="password" data-cy="login-senha" />
 
     <Button
       type="submit"
+      data-cy="login-button-signin"
       variant="primary.main"
       margin={{
         xs: '0 auto',
