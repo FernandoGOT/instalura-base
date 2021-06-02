69c0960ede45c46245427778dc97b372d67a17a2
Fernando GOT <fernando.1f@hotmail.com>
2021-06-02 12:45:58 -0300

    test(login): created the login page test
    Created a test file for the login page and implemented a basic test for its login form

---
+++ cypress/integration/pages/app/login.spec.js
@@ -0,0 +1,15 @@
+// <reference types="cypress" />
+
+import { test } from 'shelljs'
+
+describe('/pages/app/login/', () => {
+  it('preencha os campos e vá para a página /app/perfil/', () => {
+    cy.visit('/app/login/')
+
+    cy.get('[data-cy="login-usuario"]').type('fernandogot')
+    cy.get('[data-cy="login-senha"]').type('senhasegura')
+    cy.get('[data-cy="login-button-signin"]').click()
+
+    cy.url().should('include', '/app/profile/')
+  })
+})
