d9a83e58e2915c93ece3413664a64feace6d0795
Fernando GOT <fernando.1f@hotmail.com>
2021-06-02 16:54:00 -0300

    test(login): updated username
    updated username

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,7 +2,7 @@ describe('/pages/app/login/', () => {
   it('preencha os campos e vá para a página /app/perfil/', () => {
     cy.visit('/app/login/')
 
-    cy.get('[data-cy="login-usuario"]').type('fernandogot')
+    cy.get('[data-cy="login-usuario"]').type('omariosouto')
     cy.get('[data-cy="login-senha"]').type('senhasegura')
     cy.get('[data-cy="login-button-signin"]').click()
 
